c9a67fbff618bcacf4eb91cfb908db494c97e2f3
AJ Bahnken <aj.bahnken@procore.com>
2017-04-01 19:28:38 -0700

    making stuff workz betta

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -5,24 +5,8 @@ class Nav extends Component {
     return (
       <div className="navigation">
         <div className="logo">
-
           <h1>LeapChat</h1>
         </div>
-
-        <nav>
-          <div className="tab">
-            <a href="#"><i className="fa fa-2x fa-globe"></i></a>
-          </div>
-          <div className="tab">
-            <a href="#"><i className="fa fa-2x fa-comment-o"></i></a>
-          </div>
-          <div className="tab">
-            <a href="#"><i className="fa fa-2x fa-users"></i></a>
-          </div>
-          <div className="tab">
-            <a href="#"><i className="fa fa-2x fa-cog"></i></a>
-          </div>
-        </nav>
       </div>
     );
   }
