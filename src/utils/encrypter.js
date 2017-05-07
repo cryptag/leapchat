d30d9ea654e3dff23addf9ee7fede29cb6909461
Steve Phillips <steve@tryingtobeawesome.com>
2017-05-07 02:59:58 -0700

    Whitespace consistencification

--- src/utils/encrypter.js
+++ src/utils/encrypter.js
@@ -5,30 +5,30 @@ const sha384 = require('js-sha512').sha384;
 const emailDomain = '@cryptag.org';
 
 export function getEmail(passphrase){
-	return sha384(passphrase) + emailDomain;
+  return sha384(passphrase) + emailDomain;
 }
 
 export function getPassphrase(documentHash){
-    let isNewRoom = false;
+  let isNewRoom = false;
 
-	let passphrase = documentHash || '#';
-    passphrase = passphrase.slice(1);
+  let passphrase = documentHash || '#';
+  passphrase = passphrase.slice(1);
 
-    // Generate new room for user if none specified (that is, if the
-    // URL hash is blank)
-    if (!passphrase){
-      passphrase = genPassphrase();
-      isNewRoom = true;
-    }
+  // Generate new room for user if none specified (that is, if the
+  // URL hash is blank)
+  if (!passphrase){
+    passphrase = genPassphrase();
+    isNewRoom = true;
+  }
 
-    return {
-    	passphrase,
-    	isNewRoom
-    };
+  return {
+    passphrase,
+    isNewRoom
+  };
 }
 
 // TODO: Do smarter msgKey creation
 export function generateMessageKey(i){
-    let date = new Date();
-    return date.toGMTString() + ' - ' + date.getSeconds() + '.' + date.getMilliseconds() + '.' + i;
+  let date = new Date();
+  return date.toGMTString() + ' - ' + date.getSeconds() + '.' + date.getMilliseconds() + '.' + i;
 }
