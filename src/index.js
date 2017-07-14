89dc4a46c03e8a5a687bf5d2d34f78d6dafbe072
Sebastian <sebastian.lindstrom@gmail.com>
2017-07-14 08:15:22 +0200

    Changed implementation of integration with Redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, compose } from 'react-redux';
-import { createStore } from 'redux'
-import rootReducer from './reducers'
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import App from './components/App';
+import ReduxDevTools from './components/_dev/ReduxDevTools';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers());
+const enhancer = compose(
+    ReduxDevTools.instrument()
+);
+
+const store = createStore(rootReducer, enhancer);// Hot reload reducers (requires Webpack or Browserify HMR to be enabled)
+
+if (module.hot) {
+    module.hot.accept('./reducers', () =>
+        store.replaceReducer(require('./reducers')/*.default if you use Babel 6+ */)
+    );
+}
+
 
 ReactDOM.render(
     <Provider store={store}>
