06404a0cacfbd971bd228b088bfc07c06f8fe715
Sebastian <sebastian.lindstrom@gmail.com>
2017-07-16 14:30:13 +0200

    Restructured and refactored for webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App';
+import './index.html';
+import './static/lib/fetch/fetch.js';
+import './static/js/crypto/blake2s.js';
+import './static/js/crypto/nacl.js';
+import './static/js/crypto/nacl-stream.js';
+import './static/js/crypto/scrypt.js';
+import './static/js/base58.js';
+import './static/js/miniLock.js';
+import './static/js/ui.js';
+import './static/sass/main.scss';
+import './static/lib/bootstrap/dist/css/bootstrap.css';
+import './static/audio/notification_gertz.wav';
 
 ReactDOM.render(<App />, document.getElementById('root'));
