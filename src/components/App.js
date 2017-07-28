1058c0095006bd13bb01419caf966c37da63431d
Martin Yee <MartyTheeMartian@Martins-MacBook-Air.local>
2017-07-27 23:10:56 -0700

    Fixed Desktop & Mobile Responsiveness Issue

--- src/components/App.js
+++ src/components/App.js
@@ -623,7 +623,8 @@ export default class App extends Component {
     console.log('Rendering...');
 
     return (
-      <div className="encloser">
+      // <div className="encloser">
+      <div id="page">
         <Header
           statuses={statuses}
           promptForUsername={this.promptForUsername} />
