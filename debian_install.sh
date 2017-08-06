eb6e9c13f282358a26494f9eed8636124a4271cb
AJ Bahnken <aj.bahnken@procore.com>
2017-08-05 20:51:16 -0700

    Install 9.6 from postgresql.org's apt repo

--- debian_install.sh
+++ debian_install.sh
@@ -2,4 +2,7 @@
 # Steve Phillips / elimisteve
 # 2017.05.13
 
+sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
 sudo apt-get install postgresql-9.6 postgresql-contrib-9.6
