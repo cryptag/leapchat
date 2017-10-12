4b5dfcd34af93346b7b7da6f352a303f3912021d
Mo-Sho <muhama94@gmail.com>
2017-10-12 05:48:34 -0700

    extend close picker to include left panel

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,8 +4,10 @@ import UserList from '../chat/UserList';
 import Logo from './Logo';
 import Settings from './Settings';
 import Info from './Info';
+import { closePicker } from '../../actions/chatActions';
+import { connect } from 'react-redux';
 
-export default class Header extends Component {
+class Header extends Component {
   constructor(props){
     super(props);
 
@@ -17,21 +19,24 @@ export default class Header extends Component {
   }
 
   render(){
+    const { closePicker, toggleInfoModal, showSettings, statuses } = this.props;
     return (
-      <header>
+      <header onClick={closePicker}>
         <div className="logo-container">
           <div id="logo-info">
             <Logo />
-            <Info toggleInfoModal={this.props.toggleInfoModal}/>
+            <Info toggleInfoModal={toggleInfoModal}/>
           </div>
           <Settings
-            showSettings={this.props.showSettings} />
+            showSettings={showSettings} />
         </div>
         <UserIcon toggleUserList={this.toggleUserList} />
         <UserList
-          statuses={this.props.statuses}
+          statuses={statuses}
           displayUserList={this.state.displayUserList} />
       </header>
     )
   }
 }
+
+export default connect(null, { closePicker })(Header);
