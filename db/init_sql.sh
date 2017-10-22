aa05207f35be75334cffb84123faa086f4dd844b
Steve Phillips <steve@tryingtobeawesome.com>
2017-10-21 19:42:53 -0700

    Tweaked db/init_sql.sh

--- db/init_sql.sh
+++ db/init_sql.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-# Steve Phillips / elimisteve
-# 2017.05.13
 
 set -euo pipefail
 
 # Create 'leapchat' database, associated role
 psql -d postgres < sql/pre.sql
+
 export pg_user=postgres
 if [ "`uname -s`" != "Linux" ]; then
     # For Mac OS X
@@ -16,6 +15,7 @@ fi
 for file in sql/init*.sql; do
     psql -U $pg_user -d leapchat < "$file"
 done
+
 # Create tables
 for file in sql/table*.sql; do
     psql -U $pg_user -d leapchat < "$file"
