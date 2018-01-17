c7ce0b2f4e3a575838cc1d0d705d5b86d43ab7e7
Steve Phillips <steve@tryingtobeawesome.com>
2018-01-16 23:34:14 -0800

    Updated gosecure middleware

--- vendor/github.com/cryptag/gosecure/frame/frame.go
+++ vendor/github.com/cryptag/gosecure/frame/frame.go
@@ -13,7 +13,7 @@ func GetHandler(origin string) func(h http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 			w.Header().Set("X-Frame-Options", "ALLOW-FROM https://"+origin)
-			w.Header().Set("Content-Security-Policy", "frame-ancestors 'self' 'https://"+origin+"'")
+			w.Header().Set("Content-Security-Policy", "frame-ancestors 'self' https://"+origin)
 			h.ServeHTTP(w, req)
 		})
 	}
