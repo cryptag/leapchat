78883d06cc976c15360a127be0a14e9eccca9947
Steve Phillips <steve@tryingtobeawesome.com>
2023-02-13 14:13:21 -0800

    Task management: Added SQL tables for storing todo lists and tasks
    Contributes mightily to #264

---
+++ db/sql/table04_tasks.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tasks (
+    id        uuid NOT NULL UNIQUE PRIMARY KEY DEFAULT uuid_generate_v4(),
+    room_id   text NOT NULL REFERENCES rooms ON DELETE CASCADE,
+
+    -- This is called 'title_enc' (encrypted title) but stores
+    -- miniLock ciphertext, which can also store metadata
+    title_enc text             NOT NULL,  -- base64-encoded ciphertext
+
+    list_id   text             NOT NULL REFERENCES todo_lists ON DELETE CASCADE,
+    index     double precision NOT NULL   -- index in the list with list_id
+
+    -- ASSUMPTION: tasks don't expire and must be manually deleted by the user
+
+    -- ASSUMPTION: no timestamp needed; better for user metadata privacy to not store it
+);
+ALTER TABLE tasks OWNER TO superuser;
