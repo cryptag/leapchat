8d2ed1d8663f35b11a33bda506ec657894ac07f5
Jim McGaw <jpmcgaw@gmail.com>
2023-02-21 14:28:29 -0500

    Message search modal (#279)

---
+++ src/components/chat/toolbar/OpenSearchIcon.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+import { FaSearch } from 'react-icons/fa';
+
+const OpenSearchIcon = ({ onToggleModalVisibility }) => {
+
+  const openTooltip = (
+    <Tooltip id="open-message-search">Search Content</Tooltip>
+  );
+
+  const onOpenSearchModal = () => {
+    onToggleModalVisibility('search', true);
+  };
+
+  return (
+    <div className="open-message-search">
+      <OverlayTrigger overlay={openTooltip} placement="top" delayShow={300} delayHide={150}>
+        <FaSearch
+          size={24}
+          onClick={onOpenSearchModal} />
+      </OverlayTrigger>
+    </div>
+  );
+};
+
+OpenSearchIcon.propTypes = {
+  onToggleModalVisibility: PropTypes.func.isRequired
+};
+
+export default OpenSearchIcon;
---
+++ src/components/modals/SearchModal.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { Modal, Popover, OverlayTrigger } from 'react-bootstrap';
+import { FaInfoCircle } from 'react-icons/fa';
+
+import MiniSearch from 'minisearch';
+
+import Message from '../chat/Message';
+
+const searchInfoPopover = (
+  <Popover id="search-info-popover" title="Search Message History">
+    Your messages are encrypted in transit and in storage on our servers. So 
+    how can you search them?
+    <br /><br />
+    Simple! The search happens entirely in the browser. So you can rest easy.
+  </Popover>
+);
+
+
+class MessageSearchModal extends Component {
+  constructor(props) {
+    super(props);
+
+    const miniSearch = this.indexAllMessages(props.messages);
+
+    this.state = {
+      username: this.props.username,
+      isVisible: this.props.isVisible,
+      miniSearch: miniSearch,
+      isLoadingResults: false,
+      searchResults: []
+    };
+  }
+
+  indexAllMessages(messages) {
+    const miniSearch = new MiniSearch({
+      fields: ['msg', 'from'], // fields to index for full-text search
+      storeFields: ['msg', 'from'] // fields to return with search results
+    });
+
+    messages = this.props.messages.map((message, i) => {
+      return {"id": i, ...message};
+    });
+    miniSearch.addAll(messages);
+
+    return miniSearch;
+  };
+  
+  getSearchResults = (e) => {
+    this.setState({
+      isLoadingResults: true
+    });
+    let searchResults = this.state.miniSearch.search(e.target.value);
+    this.setState({
+      searchResults: searchResults,
+      isLoadingResults: false
+    });
+  }
+
+  onClose = () => {
+    // belt and suspenders; explicitly wipe local index from state
+    this.setState({ miniSearch: null });
+    this.props.onToggleModalVisibility('search', false);
+  }
+
+  render() {
+    const {
+      isVisible,
+      searchResults,
+      isLoadingResults,
+      username } = this.state;
+
+    searchResults.sort((a, b) => a.score > b.score);
+
+    return (
+      <div>
+        <Modal show={isVisible} onHide={this.onClose}>
+          <Modal.Header closeButton>
+            Search{' '}
+            <OverlayTrigger trigger="click" placement="bottom" overlay={searchInfoPopover}>
+              <FaInfoCircle size="25" />
+            </OverlayTrigger>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="form-group">
+              <form>
+                <label htmlFor="message-search">Search messages</label>
+                <input 
+                  id="message-search"
+                  type="search"
+                  className="form-control"
+                  placeholder="Enter search text" 
+                  onChange={this.getSearchResults} 
+                  autoFocus={true} />
+              </form>
+            </div>
+            <hr />
+            {isLoadingResults && <div>
+              <Throbber />
+              <h4>Loading results...</h4>
+            </div>}
+            {searchResults.length > 0 && <div className="search-results">
+              <ul>
+                {searchResults.map((result, i) => {
+                  return <Message key={i} username={username} message={result} />;
+                })}
+              </ul>
+            </div>}
+            
+  
+          </Modal.Body>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+MessageSearchModal.propTypes = {
+  username: PropTypes.string.isRequired,
+  isVisible: PropTypes.bool.isRequired,
+  onToggleModalVisibility: PropTypes.func.isRequired,
+};
+
+export default MessageSearchModal;
